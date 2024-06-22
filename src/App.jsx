d6f0aa6e5c0d056b9b62ccd259cb7029a70c46f1
emzhran <zachran750@gmail.com>
2024-06-22 12:23:49 +0700

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ function App() {
 
   return (
     <>
+    <div>
     <h1 className="text">Mohammad Zachran Zachary</h1>
     <img className="gambar" src={gambar} alt="gambar"/>
-    <h1 className="text">20220140118</h1>
+    <p className="p">20220140118</p>
     <h1 className="text">Mahasiswa Teknologi Informasi UMY</h1>
+    </div>
     </>
   )
 }
